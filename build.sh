d31cf78955af045fe99039b50309ad3edfb3e835
Dongning Wang <dongning.wang@gmail.com>
2016-12-30 22:43:27 -0600

    distilli

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ IFS=$'\n\t'
 export METEOR_SETTINGS=$(cat settings.json)
 export NODE_ENV=production
 meteor add rocketchat:internal-hubot meteorhacks:kadira
-meteor build --server https://demo.rocket.chat --directory /var/www/rocket.chat
-cd /var/www/rocket.chat/bundle/programs/server
+meteor build --server https://demo.rocket.chat --directory ~/rocket.chat
+cd ~/rocket.chat/bundle/programs/server
 npm install
-cd /var/www/rocket.chat/current
-pm2 startOrRestart /var/www/rocket.chat/current/pm2.json
+# cd /var/www/rocket.chat/current
+# pm2 startOrRestart /var/www/rocket.chat/current/pm2.json
