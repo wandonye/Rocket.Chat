01919e35debe55b02bb0837444874e54204df860
Dongning Wang <dongning.wang@gmail.com>
2017-01-01 11:46:04 -0600

    removed unnecessary

--- buildlinux.sh
+++ buildlinux.sh
@@ -5,11 +5,9 @@ IFS=$'\n\t'
 
 export METEOR_SETTINGS=$(cat settings.json)
 export NODE_ENV=production
-npm install --save babel-runtime moment toastr bcrypt dtrace-provider
+# npm install dtrace-provider
 npm install --$NODE_ENV
 meteor add rocketchat:internal-hubot meteorhacks:kadira
 meteor build --server https://chat.linknitive.com --directory ~/rocket.chat --architecture os.linux.x86_64
-cd ~/rocket.chat/
-gtar zcf linknitive.tar.gz ./bundle
 # cd /var/www/rocket.chat/current
 # pm2 startOrRestart /var/www/rocket.chat/current/pm2.json
--- buildmac.sh
+++ buildmac.sh
@@ -5,12 +5,9 @@ IFS=$'\n\t'
 
 export METEOR_SETTINGS=$(cat settings.json)
 export NODE_ENV=production
-npm install babel-runtime moment toastr bcrypt dtrace-provider
 npm install --$NODE_ENV
 meteor add rocketchat:internal-hubot meteorhacks:kadira
 meteor build --server http://localhost --directory ~/linknitive
-cd ~/linknitive/
-gtar zcf linknitive.tar.gz ./bundle
 cd ~/linknitive/bundle/programs/server
 npm install
 # cd /var/www/rocket.chat/current
