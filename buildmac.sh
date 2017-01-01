2a8725227d15debd60ccdf067dcb84d731eb8db6
Dongning Wang <dongning.wang@gmail.com>
2016-12-31 20:17:47 -0600

    distelli again

--- buildmac.sh
+++ buildmac.sh
@@ -8,8 +8,10 @@ export NODE_ENV=production
 meteor npm install --save babel-runtime moment toastr
 npm install --$NODE_ENV
 meteor add rocketchat:internal-hubot meteorhacks:kadira
-meteor build --server http://localhost --directory ~/rocket.chat
-cd ~/rocket.chat/bundle/programs/server
+meteor build --server http://localhost --directory ~/linknitive
+cd ~/linknitive/
+gtar zcf linknitive.tar.gz ./bundle
+cd ~/linknitive/bundle/programs/server
 npm install
 # cd /var/www/rocket.chat/current
 # pm2 startOrRestart /var/www/rocket.chat/current/pm2.json
